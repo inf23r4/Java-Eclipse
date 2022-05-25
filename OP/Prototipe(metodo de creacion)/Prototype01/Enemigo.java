9e347d9de37492b26ac413d48a59bd377ba4e133
inf23r4 <diazagueromario@gmail.com>
2022-05-25 15:26:17 -0300

    Prototype(Metodo de creación)

---
+++ OP/Prototipe(metodo de creacion)/Prototype01/Enemigo.java
@@ -0,0 +1,29 @@
+package Prototype01;
+
+public abstract class Enemigo
+
+{
+    private String nombre ="";
+    private String arma = "DAGA";
+    // ------------------------------
+    public String getNombre() {
+        return this.nombre;
+    }
+    // ------------------------------
+    public void setNombre( String nombre) {
+        this.nombre = nombre;
+    }
+    // ------------------------------
+    public String getArma() {
+        return this.arma;
+    }
+    // ------------------------------
+    public void setArma( String arma) {
+        this.arma = arma;
+    }
+    // ------------------------------
+    // Métodos que deberán ser construídos por las clases que hereden de ésta
+     public abstract Enemigo clonar();
+    public abstract void atacar();
+    public abstract void detener();
+}
---
+++ OP/Prototipe(metodo de creacion)/Prototype01/Guerrero.java
@@ -0,0 +1,33 @@
+package Prototype01;
+
+public class Guerrero extends Enemigo
+{
+    public Guerrero() {
+        System.out.println("Guerrero creado");
+    }
+    // ------------------------------
+    @Override
+    public void atacar() {
+        System.out.println("El guerrero ataca");
+    }
+    // ------------------------------
+    @Override
+    public void detener() {
+        System.out.println("El guerrero se detiene");
+    }
+    // ------------------------------
+    @Override
+     public Enemigo clonar()
+    {
+        /*
+         * Creamos una nueva instancia y le asignamos los valores actuales para
+         * después devolverlo (es también de tipo 'Prototype' ya que hereda de él)
+         */
+         Guerrero objGuerrero = new Guerrero();
+ 
+     objGuerrero.setNombre( this.getNombre() );
+         objGuerrero.setArma( this.getArma() );
+ 
+         return objGuerrero;
+    }
+}
