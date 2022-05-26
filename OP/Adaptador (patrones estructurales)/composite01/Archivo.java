c3f37bbc78ce36dd93af0900872f8d4ba3f35c47
inf23r4 <diazagueromario@gmail.com>
2022-05-26 05:13:09 -0300

    Composite (patron de estructura)

---
+++ OP/Adaptador (patrones estructurales)/composite01/Archivo.java
@@ -0,0 +1,16 @@
+package composite01;
+
+public class Archivo extends Nodo
+{
+    public Archivo( String nombre )
+    {
+        this.setTipoNodo( Nodo.ARCHIVO );
+        this.setNombre( nombre );
+    }
+    // ----------------------------
+    @Override
+     public void mostrar()
+    {
+        System.out.println( "Archivo: [" + this.getNombre() + "]" );
+    }
+}
